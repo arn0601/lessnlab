15cc2e7613fbd14e67a13ab263a9e05452002359
Deepak Bapat <dab352@cornell.edu>
2012-12-07 21:40:32 -0500

    slowly adding auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -10,3 +11,8 @@ class UserProfile(models.Model):
 	is_admin = models.BooleanField()
 	is_student = models.BooleanField()
 	
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
