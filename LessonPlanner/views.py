c47be5e325ac8a5cbb20a7bfdaf62de70ba9a532
AmanNalavade <amannalavade@ubuntu.ubuntu-domain>
2012-12-16 17:03:54 -0500

    Adding lessons view

--- LessonPlanner/views.py
+++ LessonPlanner/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from LessonPlanner.models import Lesson
+
+def showLesson(request):
+	creator = request.session.get('uesrname')
+	title = creator + "'s Lessons "
+	allLessons = Lesson.objects.filter(CreatorID=creator)
+	return render_to_response('lessons.html', {'allLessons':allLessons, 'title':title})
+
+
