d8f0b9141ce0022b0dcd0154cf6bbbc1e4147bc4
Deepak Bapat <dab352@cornell.edu>
2013-01-01 20:05:43 -0500

    doesnt work editing

--- LessonPlanner/views.py
+++ LessonPlanner/views.py
@@ -5,24 +5,35 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.core import serializers
+import simplejson
 @csrf_exempt
 def showLesson(request):
-	print "IN REQUEST"
     	if request.is_ajax():
-        	lessonID = request.POST.get('lID')
-		print "AJAX REQUEST",lessonID
-		content = ContentSection.objects.filter(LessonID=lessonID)
-		print content.count()
-		if content.count() == 0:
-			return HttpResponse("")
-		data = serializers.serialize('json', content, fields=('Content','SectionNumber','Header','ContentType','LessonID'))
-		print data,"","asd"
-		return HttpResponse(data)
+		action = request.POST.get('action')
+		if(int(action) == 0):
+        		lessonID = request.POST.get('lID')
+			content = ContentSection.objects.filter(LessonID=lessonID)
+			if content.count() == 0:
+				return HttpResponse("")
+			data = serializers.serialize('json', content, fields=('Content','SectionNumber','Header','ContentType','LessonID'))
+			return HttpResponse(data)
+		else:
+			print "ajax request 2"
+			jsonobj = request.POST.get('section')
+			print jsonobj
+			data = simplejson.loads(jsonobj)
+			print data['sectionNumber'], request.POST.get('lID')
+			content = ContentSection.objects.filter(LessonID=request.POST.get('lID')).filter(SectionNumber=1)
+			print content
+			content.Content=data['content']
+			print content.Content
+			content.save()
+			#data = serializers.serialize('json', content, fields=('Content','SectionNumber','Header','ContentType','LessonID'))
+			print "pushing data"
+			#return HttpResponse(data)
 	else:
 		creatorID = request.user.id
 		creatorName = request.user.username
-		print creatorID
-		print creatorName
 		title =  "'s Lessons "
 		allLessons = Lesson.objects.filter(CreatorID=creatorID)
 		return render_to_response('lessons.html', {'allLessons':allLessons, 'title':creatorName})
--- media/showLesson.js
+++ media/showLesson.js
@@ -1,16 +1,62 @@
-function createForm(myData) {
+
+function saveSection(lid, formname) {
+	try {
+	alert(formname)
+	var form = document.forms[formname];
+	var sectionNumber = form["secnum"].value;
+	var header = form["header"].value;
+	var content = form["content"].value;
+	var section = new Object()
+	section.sectionNumber=sectionNumber;
+	section.header=header;
+	section.content=content;
+	var secstring = JSON.stringify(section);
+	$.post("/lessons/", { action:1, lID:lid, section:secstring }, showLesson(lid));
+	} catch(e) { alert(e) }
+}
+function createForm(myData, lid) {
 	
 	var htmlString = "hello <br/>"
 	var count=0;
         for (var f in myData) {	
-		htmlString += "<form name=input id=" + count + ">"
 		contentSection = myData[f]['fields'];
-		htmlString += "<input type=\"text\" value=" + contentSection['SectionNumber'] + ">";
-		htmlString += "<input type=\"text\" value=" + contentSection['Header'] + ">";
-		htmlString += "<input type=\"text\" value=\"" + contentSection['Content'] + "\">";
+		htmlString += "<form name=input" + contentSection['SectionNumber'] + " method=\"post\" onsubmit=\"return saveSection(" + lid + ",'input" + contentSection['SectionNumber'] + "')\">";
+		htmlString += "<input name=secnum type=\"text\" value=" + contentSection['SectionNumber'] + ">";
+		htmlString += "<input name=header type=\"text\" value=" + contentSection['Header'] + ">";
+		htmlString += "<input name=content type=\"text\" value=\"" + contentSection['Content'] + "\">";
+		htmlString += "<input type=\"submit\" value=\"submit\">"
 		htmlString += "</form>"
 	}
 	return htmlString;
+
+}
+
+function showLesson(id) {
+
+                $.post("/lessons/", {
+                    lID:id , action:0,
+                 },
+                        function(data) {
+                                try{
+                                if(data == "") {
+                                        $('#cont').html("")
+                                        return;
+                                }
+
+                                var myData = jQuery.parseJSON(data)
+                                var htmlString = '';
+                                $.each(myData,function(){
+                                        htmlString += this['fields']['Content'] + "<br/>"
+                                });
+                                $('#cont').html(createForm(myData, id));
+                                cont = myData;
+                                }
+                                catch (e) {
+                                        alert(e);
+                                }
+                        }
+                );
+
 
 }
 
@@ -19,7 +65,7 @@ $(document).ready(function() {
         $(this).click(function() {
                 var id = $(this).attr('lID');
                 $.post("/lessons/", {
-                    lID:id ,
+                    lID:id , action:0, 
                  },
 	    		function(data) {
 				try{
@@ -34,7 +80,7 @@ $(document).ready(function() {
 				$.each(myData,function(){
 					htmlString += this['fields']['Content'] + "<br/>"
 				});
-				$('#cont').html(createForm(myData));
+				$('#cont').html(createForm(myData, id));
                 		cont = myData;
 				}
 				catch (e) {
