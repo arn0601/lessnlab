bb96107b22c272320876b1060767c5dcb2e20754
Deepak Bapat <dab352@cornell.edu>
2013-02-13 00:07:09 -0500

    accounts urls

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.contrib.auth import views as auth_views
+
+from registration.views import activate
+from registration.views import register
+from accounts.forms import UserProfileRegistrationForm
+
+urlpatterns = patterns('',
+	url(r'^register/$',register,{'backend':'registration.backends.default.DefaultBackend', 'form_class' : UserProfileRegistrationForm},name='registration.views.register')
+)
