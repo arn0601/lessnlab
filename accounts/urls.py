9d330517c9a66953f8e5e93baedd8fb88f9b5ffc
AmanNalavade <amannalavade@ubuntu.ubuntu-domain>
2013-02-14 01:25:28 -0500

    Added functionality to login

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ from registration.views import register
 from accounts.forms import UserProfileRegistrationForm
 
 urlpatterns = patterns('',
-	url(r'^register/$',register,{'backend':'registration.backends.default.DefaultBackend', 'form_class' : UserProfileRegistrationForm},name='registration.views.register')
+	url(r'^register/$',register,{'backend':'registration.backends.default.DefaultBackend', 'form_class' : UserProfileRegistrationForm},name='registration.views.register'),
+url(r'^/login/$', 'accounts.views.login_user'),
+
 )
--- accounts/views.py
+++ accounts/views.py
@@ -4,23 +4,33 @@ from django.shortcuts import render_to_response
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 
-'''
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+
+
+from django.contrib.auth.decorators import login_required
+
 @csrf_exempt
+@login_required
 def login_user(request):
-	state = "Please log in below"
-	username = password = ''
 	if request.POST:
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-		user = authenticate(username=username, password=password)
+		username = request.POST['username']
+    		password = request.POST['password']
+	    	user = authenticate(username=username, password=password)
 		if user is not None:
-			if user.is_active:
-				login(request, user)
-				state = "Successfully logged in"
+        		if user.is_active:
+            			login(request, user)
 				return HttpResponseRedirect('/lessons/')
-			else:
-				state = "Your account isnt active"
-		else:
-			state = "Something is incorrect"
-	return render_to_response('auth.html', {'state':state, 'username':username, 'password':password})
-'''
+        		
+            			# Return a 'disabled account' error message
+    		else:
+			error = "Invalid User Credentials"
+        		return render_to_response('registration/login.html', {'errors':error, 'username':username, 'password':password})
+			# Return an 'invalid login' error message.
+	else:
+		return render_to_response('registration/login.html')
+
+
