0f4ea404433a539a999f61deb287cde984397356
AmanNalavade <amannalavade@ubuntu.ubuntu-domain>
2013-02-17 20:46:15 -0500

    Added Objective Model

--- Objectives/models.py
+++ Objectives/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from Standards import standard
 # Create your models here.
+
+class Objective(models.Model):
+	name = models.charField(max_length=32)
+	standard = models.ForeignKey('Standard')
+	owner = models.charField(max_length=32)
+	course = models.ForeignKey('Course') 
+	creationDate = models.DateField()
+	parentObjective = models.ForeignKey('Objective')
+	childrenOnjectives = models.ManyToManyField(Objective)
+	description = models.TextField()
+	rating = models.PositiveIntegerField()
+
+class Course(models.model)
+	owner = models.ForiegnKey('Owner')
+	department = models.charField(max_length=32)
+	subject = models.charField(max_length=32)
+	year = models.PositiveIntegerField()
