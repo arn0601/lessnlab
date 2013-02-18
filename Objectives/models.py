97a1a44fac3fccf515cdf90d14fc5fa3db63db31
Deepak Bapat <dab352@cornell.edu>
2013-02-17 20:55:27 -0500

    fixed objectives

--- Objectives/models.py
+++ Objectives/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-from Standards import standard
+from Standards.models import Standard
+from accounts.models import UserProfile
 # Create your models here.
 
 class Objective(models.Model):
-	name = models.charField(max_length=32)
-	standard = models.ForeignKey('Standard')
-	owner = models.charField(max_length=32)
+	name = models.CharField(max_length=32)
+	standard = models.ForeignKey('Standards.Standard')
+	owner = models.ForeignKey('accounts.UserProfile')
 	course = models.ForeignKey('Course') 
 	creationDate = models.DateField()
-	parentObjective = models.ForeignKey('Objective')
-	childrenOnjectives = models.ManyToManyField(Objective)
+	parentObjective = models.ForeignKey('self')
+	childrenOnjectives = models.ManyToManyField('self')
 	description = models.TextField()
 	rating = models.PositiveIntegerField()
 
-class Course(models.model)
-	owner = models.ForiegnKey('Owner')
-	department = models.charField(max_length=32)
-	subject = models.charField(max_length=32)
+class Course(models.Model):
+	owner = models.ForeignKey('accounts.UserProfile')
+	department = models.CharField(max_length=32)
+	subject = models.CharField(max_length=32)
 	year = models.PositiveIntegerField()
