76d493b033d65cb4fca8d625ad3e1f7a9ab7c7e6
Deepak Bapat <dab352@cornell.edu>
2013-02-23 16:24:23 -0500

    css working

--- DisruptED/settings.py
+++ DisruptED/settings.py
@@ -66,6 +66,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)),'..'),'static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
