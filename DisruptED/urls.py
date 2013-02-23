a99dc937d0786f25a0e3ead79e001c216650926f
Deepak Bapat <dab352@cornell.edu>
2013-02-23 17:32:32 -0500

    mayeb

--- DisruptED/urls.py
+++ DisruptED/urls.py
@@ -16,9 +16,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 #    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^login/$', 'accounts.views.login_user'),
     (r'^accounts/', include('accounts.urls')),
     (r'^accounts/', include('registration.backends.default.urls')),
-    (r'^login/$', 'accounts.views.login_user'),
     url(r'^lessons/$', 'LessonPlanner.views.showLesson'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
            'document_root': settings.MEDIA_ROOT,
