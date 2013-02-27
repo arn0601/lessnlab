c3a603c4e4ad91df6731951eeaa820085502b839
Deepak Bapat <dab352@cornell.edu>
2013-02-27 00:01:07 -0500

    fix for model change

--- Objectives/models.py
+++ Objectives/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from Standards.models import Standard
 from accounts.models import UserProfile
+from LessonPlanner.models import Course
 # Create your models here.
 
 class Objective(models.Model):
 	name = models.CharField(max_length=32)
 	standard = models.ForeignKey('Standards.Standard')
 	owner = models.ForeignKey('accounts.UserProfile')
-	course = models.ForeignKey('Course') 
+	course = models.ForeignKey('LessonPlanner.Course') 
 	creationDate = models.DateField()
 	parentObjective = models.ForeignKey('self')
 	childrenOnjectives = models.ManyToManyField('self')
