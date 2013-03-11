d17d2104ab963d8dd1c887fb03b4b9418b49a94c
Deepak Bapat <dab352@cornell.edu>
2013-03-11 00:04:28 -0400

    Added standards to form, now need to populate on submit

--- Standards/models.py
+++ Standards/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+STATE_CHOICES = [('', 'None'),('PA','Pennsylvania'), ('MO', 'Missouri'), ('NY', 'New York')]
+
 class Standard(models.Model):
 	name = models.CharField(max_length=32)
-	owner_type = models.CharField(max_length=32)
-	description = models.CharField(max_length=32)
+	owner_type = models.CharField(max_length=32, choices=STATE_CHOICES)
+	description = models.TextField()
 	creation_date = models.DateTimeField()
 	start_date = models.DateField()
 	expiration_date = models.DateField()
 	department = models.CharField(max_length=32)
 	subject = models.CharField(max_length=32)
 	grade = models.CharField(max_length=32)
-
 
 
 	
