3fb488677c179b5a5cee89c62f046a9939641c24
Deepak Bapat <dab352@cornell.edu>
2013-03-17 22:35:16 -0400

    trying

--- LessonPlanner/templatetags/dictionary_extras.py
+++ LessonPlanner/templatetags/dictionary_extras.py
@@ -1,6 +1,29 @@
 from django import template
 register = template.Library()
 
+@register.filter(name='isdict')
+def isdict(value):
+	if isinstance(value, dict):
+		return 'T'
+	return 'F'
+
 @register.filter(name='access')
 def access(value, arg):
-    return value[arg]
+	print value
+	
+	if isinstance(value.get(arg),dict):
+		new_dict = {}
+		print 'it is a dictionary'
+		for key in value[arg].keys():
+			new_dict[key] = value[arg][key]
+		return new_dict
+	
+	return value.get(arg)
+
+@register.filter(name='sort_sections')
+def sort_sections(value):
+	l = []
+	for key in value.keys():
+		l.append((key, key.placement))
+	sorted(l, key = lambda x: x[1])
+	return l
