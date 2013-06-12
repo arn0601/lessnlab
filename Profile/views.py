c7bf63ae3dbc8e2bb96424ab4f6a324127d30e65
AmanNalavade <amannalavade@ubuntu.ubuntu-domain>
2013-06-12 01:11:11 -0400

    Add Profile App

---
+++ Profile/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.shortcuts import render_to_response,render
+from LessonPlanner import base_methods
+
+def view_profile(request):
+	base_dict = base_methods.createBaseDict(request)
+        return render(request,"profile.html",base_dict)
+
