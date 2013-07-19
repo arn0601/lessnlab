f91fe29298e1c4b00ac9f1d2e03f0a23ce87bdbd
AmanNalavade <amannalavade@ubuntu.ubuntu-domain>
2013-07-18 22:00:21 -0400

    JS changes

--- static/lessnlab.js
+++ static/lessnlab.js
@@ -1,3 +1,110 @@
+			function goToByScroll(id){
+	 			$('html,body').animate({scrollTop: $("#"+id).offset().top - 100},1500);
+			}
+			
+
+function getVideoIDfromLink(url)
+			{
+					var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+					var match = url.match(regExp);
+					if (match&&match[2].length==11){
+							return match[2];
+					}else{
+							//error
+					}
+			};
+
+			function getVideoHtmlbyLink(video_link,div_id)
+			{
+				video_id = getVideoIDfromLink(video_link);
+				return getVideoHtmlbyID(video_id,div_id);
+			};
+
+			function getVideoHtmlbyID(video_id,div_id)
+			{
+				jQTubeUtil.video(video_id,function(response)
+				{
+					var video = response.videos[0];
+					jQuery("#"+div_id).html(getVideoHtml(video));
+				});
+			};
+
+			function getVideoHtml(video)
+			{
+				var title = video.title.substring(0,32);
+				var description = video.description.substring(0,32);
+				var video_id = video.videoId;
+				var url = "http://www.youtube.com/watch?feature=player_embedded&v=" + video_id;
+				return "<div style='width:330px;'> <div style='width:175px; float:right'> <b>Title: </b> <a href='" + url + "' > "  + title + "</a><div> <b> Description: </b>" + description +"</div> </div> <div onclick='onVidImgClick(this)' > <img src='http://img.youtube.com/vi/" + video_id + "/1.jpg' alt='splash' width='120px' height='90px' style='cursor: pointer' style='display:inline'/ ></div> <div style='display: none'>   <iframe width='200px' height='150px' temp='http://www.youtube.com/embed/" + video_id + "?autoplay=0'> </iframe></div>";
+
+			};
+
+(function() {
+    var
+        fullScreenApi = {
+            supportsFullScreen: false,
+            isFullScreen: function() { return false; },
+            requestFullScreen: function() {},
+            cancelFullScreen: function() {},
+            fullScreenEventName: '',
+            prefix: ''
+        },
+        browserPrefixes = 'webkit moz o ms khtml'.split(' ');
+ 
+    // check for native support
+    if (typeof document.cancelFullScreen != 'undefined') {
+        fullScreenApi.supportsFullScreen = true;
+    } else {
+        // check for fullscreen support by vendor prefix
+        for (var i = 0, il = browserPrefixes.length; i < il; i++ ) {
+            fullScreenApi.prefix = browserPrefixes[i];
+ 
+            if (typeof document[fullScreenApi.prefix + 'CancelFullScreen' ] != 'undefined' ) {
+                fullScreenApi.supportsFullScreen = true;
+ 
+                break;
+            }
+        }
+    }
+ 
+    // update methods to do something useful
+    if (fullScreenApi.supportsFullScreen) {
+        fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
+ 
+        fullScreenApi.isFullScreen = function() {
+            switch (this.prefix) {
+                case '':
+                    return document.fullScreen;
+                case 'webkit':
+                    return document.webkitIsFullScreen;
+                default:
+                    return document[this.prefix + 'FullScreen'];
+            }
+        }
+        fullScreenApi.requestFullScreen = function(el) {
+            return (this.prefix === '') ? el.requestFullScreen() : el[this.prefix + 'RequestFullScreen']();
+        }
+        fullScreenApi.cancelFullScreen = function(el) {
+            return (this.prefix === '') ? document.cancelFullScreen() : document[this.prefix + 'CancelFullScreen']();
+        }
+    }
+ 
+    // jQuery plugin
+    if (typeof jQuery != 'undefined') {
+        jQuery.fn.requestFullScreen = function() {
+ 
+            return this.each(function() {
+                if (fullScreenApi.supportsFullScreen) {
+                    fullScreenApi.requestFullScreen(this);
+                }
+            });
+        };
+    }
+ 
+    // export api
+    window.fullScreenApi = fullScreenApi;
+})();
+
 
 $(document).ajaxSend(function(event, xhr, settings) {
     function getCookie(name) {
