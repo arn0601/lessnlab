d3d65d1800cddc357d5047d03f0c14acddeecd29
Deepak Bapat <dab352@cornell.edu>
2013-11-05 22:57:00 -0500

    standards

--- Utils/management/commands/add_standards.py
+++ Utils/management/commands/add_standards.py
@@ -22,32 +22,38 @@ class Command(NoArgsCommand):
 				else:
 					rownum+=1
 					numbering = row[0]
-					grade = row[1]
-					if grade == "":
+					g = row[1]
+					if g == "":
 						rownum+=1
 						continue
-					stype = row[4]
-					state = row[2]
-					subject = row[5]
-					description = row[8]
-					standard = Standard()
-					try:
-						standard.standard_type = StandardType.objects.get(value=stype)
-						if (stype == 'State'):
-							standard.state = State.objects.get(value=state)
-						standard.description = description
-						standard.creation_date = datetime.datetime.today()
-						standard.expiration_date = datetime.datetime.today()
-						standard.start_date = datetime.datetime.today()
-						standard.department = Subject.objects.get(value=subject)
-						standard.subject = Subject.objects.get(value=subject)
-						standard.grade = Grade.objects.get(value=grade)
-						standard.numbering = numbering
-						standard.save()
-						sg, created = StandardGrouping.objects.get_or_create(subject=standard.subject, grade=standard.grade, standard_type=standard.standard_type, state=standard.state, prebuilt=True)
-						sg.standard.add(standard)
-					except:
-						print grade
-					print rownum
+					grade_list = [g]
+					if '-' in g:
+						splitted = g.split('-')
+						grade_list = splitted
+					for grade in grade_list:
+						stype = row[4]
+						state = row[2]
+						subject = row[5]
+						description = row[8]
+						standard = Standard()
+						try:
+							standard.standard_type = StandardType.objects.get(value=stype)
+							if (stype == 'State'):
+								standard.state = State.objects.get(value=state)
+							standard.description = description
+							standard.creation_date = datetime.datetime.today()
+							standard.expiration_date = datetime.datetime.today()
+							standard.start_date = datetime.datetime.today()
+							standard.department = Subject.objects.get(value=subject)
+							standard.subject = Subject.objects.get(value=subject)
+							standard.grade = Grade.objects.get(value=grade)
+							standard.numbering = numbering
+							standard.save()
+							sg, created = StandardGrouping.objects.get_or_create(subject=standard.subject, grade=standard.grade, standard_type=standard.standard_type, state=standard.state, prebuilt=True)
+							sg.standard.add(standard)
+						except:
+							print g
+							traceback.print_exc(file=sys.stdout)
+						print rownum
 		except:
 			traceback.print_exc(file=sys.stdout)
